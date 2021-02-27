53f45d258b0e940a305a9d56605933df7ae8b67c
gradedproject <gradedproject1@gmail.com>
2021-02-27 22:13:51 +0000

    removed react-infinite-scroll-component

--- pages/accountList.js
+++ pages/accountList.js
@@ -1,74 +1,69 @@
-import dbConnect from '../components/connection/database';
-import AccountingModel from '../components/models/accounting';
+import dbConnect from "../components/connection/database";
+import AccountingModel from "../components/models/accounting";
 
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { useState } from "react";
+import Link from "next/link";
 
-import React, {useState} from 'react'
-import Link from 'next/link';
-
-
-const AccountLists = ({posts}) => {
-  const [page, setPge] = useState(0)
-    return (
-        <div>
-                <div className="all-title-box">
+const AccountLists = ({ posts }) => {
+  const [page, setPge] = useState(0);
+  return (
+    <div>
+      <div className="all-title-box">
         <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <h2>ACCOUNTING PROJECT TOPICS AND MATERIALS</h2>
-                    <ul className="breadcrumb">
-                        <li className="breadcrumb-item"><a href="#">ACCOUNTING</a></li>
-                        <li className="breadcrumb-item active">PROJECT TOPICS LISTS</li>
-                    </ul>
-                </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <h2>ACCOUNTING PROJECT TOPICS AND MATERIALS</h2>
+              <ul className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <a href="#">ACCOUNTING</a>
+                </li>
+                <li className="breadcrumb-item active">PROJECT TOPICS LISTS</li>
+              </ul>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
 
-    
-    
-
-
-    <div className="services-box-main">
-     <div className="container">
-         <div className="row "> 
-        
-       {posts.map((document, index) => ( 
-   
- <div key={index } className="col-sm-12 col-lg-12">
-                 <div key={index } className="service-block-inner">
-                 <h4 className="service-title">
-                 <Link key={index } className="list-group-item " href={`${document._id}`}><a>{document.title}</a></Link>
-                 
-               </h4>
+      <div className="services-box-main">
+        <div className="container">
+          <div className="row ">
+            {posts.map((document, index) => (
+              <div key={index} className="col-sm-12 col-lg-12">
+                <div key={index} className="service-block-inner">
+                  <h4 className="service-title">
+                    <Link
+                      key={index}
+                      className="list-group-item "
+                      href={`${document._id}`}>
+                      <a>{document.title}</a>
+                    </Link>
+                  </h4>
                 </div>
- </div>
-
+              </div>
             ))}
-            
-         
-      
-</div>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col text-center">
-    <button onClick={loadmore} style={{fontSize: '34px', color: "red"}} class="btn btn-default b">Load More</button>
-    </div>
-  </div>
-</div>
-</div>
-
-
+          </div>
         </div>
-    )
-}
-export default AccountLists
+        <div class="container">
+          <div class="row">
+            <div class="col text-center">
+              <button
+                onClick={loadmore}
+                style={{ fontSize: "34px", color: "red" }}
+                class="btn btn-default b">
+                Load More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AccountLists;
 
-function loadmore(){
+function loadmore() {
   console.log("conneting");
 }
-
 
 export async function getStaticProps(context) {
   let networkErr;
@@ -81,10 +76,8 @@ export async function getStaticProps(context) {
 
   if (!networkErr) {
     posts = await AccountingModel.find({}).limit(250);
-  
 
     posts = JSON.parse(JSON.stringify(posts));
-
   }
   return {
     props: {
