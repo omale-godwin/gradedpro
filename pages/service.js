e81439f072f2e33f7784c9e7debd9ff64e38eda8
gradedproject <gradedproject1@gmail.com>
2021-02-27 22:09:53 +0000

    added loader to accountinga

--- pages/service.js
+++ pages/service.js
@@ -1,98 +1,231 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 export default function Service() {
-    return (
-        <div className="page-services">
-    
+  return (
+    <div className="page-services">
+      <div className="services-box-main">
+        <div className="container">
+          <div className="row my-5">
+            <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>OUR MISSION</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>OUR VISION</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>OUR PHILOSOPHY</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>We are Trusted</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>We are Professional</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>OUR STORIES</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
 
-      <div className='container-fluid mx-auto mt-5 mb-5 col-12' style={{textAlign: 'center'}}>
-
-    <h2 className="hd service-desc">Why Students Believe in Us</h2>
-    <p><small className="text-muted">We Always render more and better service than <br />is expected of Us, no matter what your ask may be.</small></p>
-    <div className="row" style={{justifyContent: 'center'}}>
-        <div className="card col-md-3 col-12">
-            <div className="card-content">
-                <div className="card-body"> <img className="img" src="https://i.imgur.com/S7FJza5.png" />
-                    <div className="shadow"></div>
-                    <div className="card-title"> Find Or Request Source Code Of Any WebApp or mobile App With Chapter 1-5 Docx </div>
-                    <div className="card-subtitle">
-                        <p> <small className="text-muted">We spent thousands of hours creating on algorithm that does this for you in seconds. We collect a small fee from the professional after they meet your need</small> </p>
-                        <Link href="/source" className="serbtn btn btn-primary">Source Code</Link>
-                        
-                    </div>
-                </div>
+          <div className="row my-4">
+            <div className="col-12">
+              <h2 className="noo-sh-title">Meet Our Team</h2>
             </div>
-        </div>
-        <div className="card col-md-3 col-12 ml-2">
-            <div className="card-content">
-                <div className="card-body"> <img className="img" src="https://i.imgur.com/xUWJuHB.png" />
-                    <div className="card-title">  </div>
-                    <div className="card-subtitle">
-                        <p> <small className="text-muted">  </small> </p>
-                        <Link href="/contacts" className="serbtn btn btn-primary"><a>Make Enquiry</a></Link>
-                    </div>
+            <div className="col-sm-6 col-lg-3">
+              <div className="hover-team">
+                <div className="our-team">
+                  {" "}
+                  <img src="images/img-1.jpg" alt="" />
+                  <div className="team-content">
+                    <h3 className="title">Williamson</h3>{" "}
+                    <span className="post">Web Developer</span>{" "}
+                  </div>
+                  <ul className="social">
+                    <li>
+                      <a href="#" className="fab fa-facebook"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-twitter"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-google-plus"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-youtube"></a>
+                    </li>
+                  </ul>
+                  <div className="icon">
+                    {" "}
+                    <i className="fa fa-plus" aria-hidden="true"></i>{" "}
+                  </div>
                 </div>
-            </div>
-        </div>
-        <div className="card col-md-3 col-12 ml-2">
-            <div className="card-content">
-                <div className="card-body"> <img className="img rck" src="https://i.imgur.com/rG3CGn3.png" />
-                    <h4 className="card-title"> Hire A Writer</h4>
-                    <div className="card-subtitle">
-                        <p> <small className="text-muted service-desc">Buying or selling a home is often the largest transaction anyone does in their life. we guide you through the process so that you can be confident in reaching your dream outcome.</small> </p>
-                        <Link href="/hirewriter" className="serbtn btn-primary btn btn-primary">Hire A Writer</Link>
-                    </div>
+                <div className="team-description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent urna diam, maximus ut ullamcorper quis, placerat id
+                    eros. Duis semper justo sed condimentum rutrum. Nunc
+                    tristique purus turpis. Maecenas vulputate.{" "}
+                  </p>
                 </div>
+                <hr className="my-0" />{" "}
+              </div>
             </div>
+            <div className="col-sm-6 col-lg-3">
+              <div className="hover-team">
+                <div className="our-team">
+                  {" "}
+                  <img src="images/img-2.jpg" alt="" />
+                  <div className="team-content">
+                    <h3 className="title">Kristiana</h3>{" "}
+                    <span className="post">Web Developer</span>{" "}
+                  </div>
+                  <ul className="social">
+                    <li>
+                      <a href="#" className="fab fa-facebook"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-twitter"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-google-plus"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-youtube"></a>
+                    </li>
+                  </ul>
+                  <div className="icon">
+                    {" "}
+                    <i className="fa fa-plus" aria-hidden="true"></i>{" "}
+                  </div>
+                </div>
+                <div className="team-description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent urna diam, maximus ut ullamcorper quis, placerat id
+                    eros. Duis semper justo sed condimentum rutrum. Nunc
+                    tristique purus turpis. Maecenas vulputate.{" "}
+                  </p>
+                </div>
+                <hr className="my-0" />{" "}
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-3">
+              <div className="hover-team">
+                <div className="our-team">
+                  {" "}
+                  <img src="images/img-3.jpg" alt="" />
+                  <div className="team-content">
+                    <h3 className="title">Steve Thomas</h3>{" "}
+                    <span className="post">Web Developer</span>{" "}
+                  </div>
+                  <ul className="social">
+                    <li>
+                      <a href="#" className="fab fa-facebook"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-twitter"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-google-plus"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-youtube"></a>
+                    </li>
+                  </ul>
+                  <div className="icon">
+                    {" "}
+                    <i className="fa fa-plus" aria-hidden="true"></i>{" "}
+                  </div>
+                </div>
+                <div className="team-description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent urna diam, maximus ut ullamcorper quis, placerat id
+                    eros. Duis semper justo sed condimentum rutrum. Nunc
+                    tristique purus turpis. Maecenas vulputate.{" "}
+                  </p>
+                </div>
+                <hr className="my-0" />{" "}
+              </div>
+            </div>
+            <div className="col-sm-6 col-lg-3">
+              <div className="hover-team">
+                <div className="our-team">
+                  {" "}
+                  <img src="images/img-1.jpg" alt="" />
+                  <div className="team-content">
+                    <h3 className="title">Williamson</h3>{" "}
+                    <span className="post">Web Developer</span>{" "}
+                  </div>
+                  <ul className="social">
+                    <li>
+                      <a href="#" className="fab fa-facebook"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-twitter"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-google-plus"></a>
+                    </li>
+                    <li>
+                      <a href="#" className="fab fa-youtube"></a>
+                    </li>
+                  </ul>
+                  <div className="icon">
+                    {" "}
+                    <i className="fa fa-plus" aria-hidden="true"></i>{" "}
+                  </div>
+                </div>
+                <div className="team-description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent urna diam, maximus ut ullamcorper quis, placerat id
+                    eros. Duis semper justo sed condimentum rutrum. Nunc
+                    tristique purus turpis. Maecenas vulputate.{" "}
+                  </p>
+                </div>
+                <hr className="my-0" />{" "}
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  
-    <div className="row" style={{justifyContent: 'center'}}>
-        <div className="card col-md-3 col-12">
-            <div className="card-content">
-                <div className="card-body"> <img className="img" src="https://i.imgur.com/S7FJza5.png" />
-                    <div className="shadow"></div>
-                    <div className="card-title"> Paraphrasing Tool (project Rewriter)</div>
-                    <div className="card-subtitle">
-                        <p> <small className="text-muted">We spent thousands of hours creating on algorithm that does this for you in seconds. We collect a small fee from the professional after they meet your need</small> </p>
-                        <button className="serbtn btn btn-primary">Rewrite Text</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="card col-md-3 col-12 ml-2">
-            <div className="card-content">
-                <div className="card-body"> <img className="img" src="https://i.imgur.com/xUWJuHB.png" />
-                    <div className="card-title"> Request Any Department or Course Project Topic Material
- </div>
-                    <div className="card-subtitle">
-                        <p> <small className="text-muted"> We don't accept ads from anyone. We use actual data href match you who the best person for each job </small> </p>
-                        <Link href="/request" className="serbtn btn btn-primary">Make Request</Link>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="card col-md-3 col-12 ml-2">
-            <div className="card-content">
-                <div className="card-body"> <img className="img rck" src="https://i.imgur.com/rG3CGn3.png" />
-                    <div className="card-title"> Make Money Online Selling Project Materials</div>
-                    <div className="card-subtitle">
-                        <p> <small className="text-muted">Buying or selling a home is often the largest transaction anyone does in their life. we guide you through the process so that you can be confident in reaching your dream outcome.</small> </p>
-                        <button className="serbtn btn btn-primary">Sell Now</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div className="ft">
-        <p className="chk"><small className="text-muted">Still not sure?</small></p>
-        <Link href="/" className="btn btn-primary">View All Department Project</Link>
-    </div>
-</div>
-    </div>
-
-        )
+  );
 }
