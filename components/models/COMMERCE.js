d082cbaac118b96a84cd3aaaed8c7f4ba20c1a63
gradedproject <gradedproject1@gmail.com>
2021-05-14 17:17:46 +0000

    api

--- components/models/COMMERCE.js
+++ components/models/COMMERCE.js
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
 
-const COMMERCEModel = new mongoose.Schema({
-  title: {
-    type: String,
-  },
-  content: {
-    type: String,
-  },
+const COMMERCEModel = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
 
-  id: {
-    type: Number,
+    id: {
+      type: Number,
+    },
   },
-});
+  {
+    collection: "User",
+    versionKey: false, //here
+  }
+);
 
 export default mongoose.models.COMMERCES ||
   mongoose.model("COMMERCES", COMMERCEModel);
