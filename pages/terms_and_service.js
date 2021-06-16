3fdc4d6a813d9027dd20b9fcde50244474284231
gradedproject <gradedproject1@gmail.com>
2021-06-16 17:25:03 +0000

    trem s of condition

---
+++ pages/terms_and_service.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function Logout() {
+  return (
+    <div className="container">
+      <h1>Terms of Use of Project by gradedproject</h1>
+      <h4>
+        GRADEDPROJECT.COM only provide Research Project Writing guides/tips with
+        project topics and materials for project writing guide, we do NOT
+        support students to download project materials on this site and submit
+        to any higher institutions for any form of award (s). To help foster and
+        develop a sustainable mindset for a better academic system for Nigerian
+        student carrying out project work, NOTE please carefully read our terms
+        of use and policies concerning materials on this site.
+      </h4>
+      <h4>
+        1 . All project materials on this website are NOT for submission to any
+        higher institution for academic awards. They are solely meant to guide
+        final year students on the various processes and methodologies of
+        writing a research project.
+      </h4>
+      <h4>
+        2. For academic purpose, it is illegal for any student to download
+        research projects on this website site and submit to any higher
+        institution in Nigeria for academic awards. Students can stand the risk
+        of being expelled if caught by project supervisors. To get a risk-free
+        customized research project
+      </h4>
+      <h4>
+        3. VISIT
+        <a href="https://Gradedproject.com./hireWriter/">
+          Gradedproject.com/hirewriter
+        </a>{" "}
+        for customize writing!
+      </h4>
+      <h4>
+        4. All research projects uploaded for free editing on this website are
+        not uploaded for downloads. this feature is solely meant for students
+        who face difficulties accessing their project supervisors.
+      </h4>
+      <h2>OUR REFUND POLICY </h2>
+      Dear esteem clients all materials on our website are fully ready for
+      student and researchers and any purchase that we cant send between the
+      time we put on our website just be cool. we are developing it. you will
+      receive it.
+      <h4>1. Thanks we dont scam </h4>
+      <h4>
+        2. Just know after 10 days of not sending your material, we can make a
+        refund.{" "}
+      </h4>
+      <h4>3. All purchase materials from client expires after 7 days.</h4>
+      <h4>
+        {" "}
+        4. After 3 days of materials purchase without calls or sms. Note we
+        shall note send again.
+      </h4>
+      <h4>
+        5. All hire a writer research work. Note if after 10 days of payment we
+        have not sent any chapter, request for refund.{" "}
+      </h4>{" "}
+      <h4>
+        6. All hire a writer research if we have sent you any chapter we will
+        not make any refund. we can only do corrections, share 50 – 50 lost if
+        the topic is change.
+      </h4>
+    </div>
+  );
+}
