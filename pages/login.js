61aab8e74038c5901beb1f44daa839a2fcf6bb76
gradedproject <gradedproject1@gmail.com>
2021-06-16 05:24:52 +0000

    removed login

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,5 @@
 import React from "react";
 
 export default function Login() {
-  return (
-    <div>
-      <ins
-        class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-3951180030872988"
-        data-ad-slot="4262502779"
-        data-ad-format="auto"
-        data-full-width-responsive="true">
-        jjjjjjjjjjjjjjjjjj
-      </ins>
-    </div>
-  );
+  return <div></div>;
 }
